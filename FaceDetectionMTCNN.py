68d5a41fd12138d7c6804fae2272da9bfeb6e312
wildangbudhi <wildangb@gmail.com>
2019-12-12 01:58:07 +0700

    Upload

---
+++ FaceDetectionMTCNN.py
@@ -0,0 +1,37 @@
+from keras.models import load_model
+import cv2
+from mtcnn.mtcnn import MTCNN
+import os
+
+base_dir = os.path.dirname(__file__)
+
+detector = MTCNN()
+
+count = 0
+
+if not os.path.exists('faces'):
+    print("New directory created")
+    os.makedirs('faces')
+
+for file in os.listdir(base_dir + '/video'):
+    video = cv2.VideoCapture("{}/video/{}".format(base_dir,file))
+    print(file)
+    
+    if(not video.isOpened()): print('Error Opning Video File')
+
+    while(video.isOpened()):
+        ret, image = video.read()
+        result = detector.detect_faces(image)
+
+        if ret :
+            for face in result:
+                print(count)
+                box = face['box']
+                img = image[box[1]:(box[1] + box[3]), box[0]:(box[0] + box[2])]
+                count += 1
+                try:
+                    cv2.imwrite(base_dir + '/faces/' + str(count) + '.jpg', img)
+                except:
+                    print('Error Saving Image')
+
+        else: break
---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import numpy as np
+import cv2
+from keras.models import load_model
+from mtcnn.mtcnn import MTCNN
+
+base_dir = os.path.dirname(__file__)
+
+class FaceRecognition:
+    def __init__(self):
+        self.detector = MTCNN()
+        self.model = load_model(base_dir + '/facenet/model/facenet_keras.h5')
+    
+    def detectFaces(self, frame :np.ndarray) -> np.ndarray:
+        faces = self.detector.detect_faces(frame)
+        # x1 x2 y1 y2
+        return np.array([ [ face['box'][0], ( face['box'][0] + face['box'][2] ), face['box'][1], ( face['box'][1] + face['box'][3] ) ] for face in faces ], dtype=float)
+    
+    def makeReactangleFaces(self, frame :np.ndarray, rect :np.ndarray):
+        cv2.rectangle(frame, ( rect[0], rect[1] ), ( rect[2], rect[3] ), ( 0, 155, 255 ), 2)
+    
+    def saveFaceFromFrame(self, path :str, frame :np.ndarray):
+        cv2.imwrite(path, frame)
+
+    # Kurang Train Model, Deteksi, dll

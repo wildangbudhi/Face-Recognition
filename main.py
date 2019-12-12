7a768e9dbad50cb31228fc7e49d3ee963f1bd8a7
wildangbudhi <wildangb@gmail.com>
2019-12-12 23:38:27 +0700

    Update Dataset and Code

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import cv2
 from keras.models import load_model
 from mtcnn.mtcnn import MTCNN
 import tensorflow as tf
+from numpy import load
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
+from sklearn.svm import SVC
 
 base_dir = os.path.dirname(__file__)
 
@@ -24,8 +29,15 @@ class FaceRecognition:
     def saveFaceFromFrame(self, path :str, frame :np.ndarray):
         cv2.imwrite(path, frame)
     
+    def embedding(self, image :np.ndarray):
+        mean, std = image.mean(), image.std()
+        image = (image - mean) / std
+        image = np.expand_dims(image, axis=0)
+        image = self.model.predict(image)
+        return image[0]
+    
     def loadDatasetAndEmbedding(self, path :str, filename :str):
-        print('Load Dataset and Compressing')
+        print('Load Dataset, Emmbedding and Write to File')
 
         X, Y = [], []
 
@@ -33,10 +45,7 @@ class FaceRecognition:
             for file in os.listdir(path + label + '/'):
                 image = cv2.imread(path + label + '/' + file)
                 image = cv2.resize(image, (160, 160), interpolation=cv2.INTER_AREA)
-                mean, std = image.mean(), image.std()
-                image = (image - mean) / std
-                image = np.expand_dims(image, axis=0)
-                image = self.model.predict(image)
+                image = self.embedding(image)
 
                 X.append( image )
                 Y.append( label )
@@ -45,6 +54,11 @@ class FaceRecognition:
         
         print('Compressed File saved as {}'.format(base_dir + '/CompressedImages/' + filename))
         np.savez_compressed(base_dir + '/CompressedImages/' + filename, X=np.asarray(X), Y=np.asarray(Y))
+    
+    def predict(self):
+        data = np.load(base_dir + '/images/Labeled/', 'dataset.npz')
+        X_Train, Y_Train = data['X'], data['Y']
+        
 
 def main():
     a = FaceRecognition()

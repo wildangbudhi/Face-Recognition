81a5775bcd697d5b7affea8fe02bb885637a2b38
wildangbudhi <wildangb@gmail.com>
2019-12-13 08:18:12 +0700

    Update

--- FaceDetectionMTCNN.py
+++ FaceDetectionMTCNN.py
@@ -13,25 +13,41 @@ if not os.path.exists('faces'):
     print("New directory created")
     os.makedirs('faces')
 
-for file in os.listdir(base_dir + '/video'):
-    video = cv2.VideoCapture("{}/video/{}".format(base_dir,file))
-    print(file)
+count = 0
+
+for file in os.listdir(base_dir + '/image'):
+    image = cv2.imread(base_dir + '/image/' + file)
+    result = detector.detect_faces(image)
+
+    for face in result:
+        box = face['box']
+        img = image[box[1]:(box[1] + box[3]), box[0]:(box[0] + box[2])]
+        try:
+            cv2.imwrite(base_dir + '/faces/' + str(count) + '.jpg', img)
+            print(count)
+        except:
+            print('Error Saving Image')
+        count += 1
+
+# for file in os.listdir(base_dir + '/video'):
+#     video = cv2.VideoCapture("{}/video/{}".format(base_dir,file))
+#     print(file)
     
-    if(not video.isOpened()): print('Error Opning Video File')
+#     if(not video.isOpened()): print('Error Opning Video File')
 
-    while(video.isOpened()):
-        ret, image = video.read()
-        result = detector.detect_faces(image)
+#     while(video.isOpened()):
+#         ret, image = video.read()
+#         result = detector.detect_faces(image)
 
-        if ret :
-            for face in result:
-                print(count)
-                box = face['box']
-                img = image[box[1]:(box[1] + box[3]), box[0]:(box[0] + box[2])]
-                count += 1
-                try:
-                    cv2.imwrite(base_dir + '/faces/' + str(count) + '.jpg', img)
-                except:
-                    print('Error Saving Image')
+#         if ret :
+#             for face in result:
+#                 print(count)
+#                 box = face['box']
+#                 img = image[box[1]:(box[1] + box[3]), box[0]:(box[0] + box[2])]
+#                 count += 1
+#                 try:
+#                     cv2.imwrite(base_dir + '/faces/' + str(count) + '.jpg', img)
+#                 except:
+#                     print('Error Saving Image')
 
-        else: break
+#         else: break
